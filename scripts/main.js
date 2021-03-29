71ee8fc616947daaf93e094bfa2df54409238701
Lucas <lxavier.00@outlook.com>
2021-03-28 21:19:02 -0300

    Add Scripts de plugin de slide

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: true, // pausa a transição automática
+    });
+    
+    new SimpleSlide({
+        slide: 'portfolio', // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true,
+        pauseOnHover: true, // pausa a transição automática
+    });
+}
